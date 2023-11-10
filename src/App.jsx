c3c21024c3eff1219584079a66c5d62948ee61ba
Bekka-447 <122794330+Bekka-447@users.noreply.github.com>
2023-11-10 23:10:18 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [openModal, setOpenModal] = useState(false)
+
+  return (
+    <>
+      <div className="container">
+        <button
+          onClick={() => setOpenModal(!openModal)}
+        >OpenModal</button>
+
+      </div>
+      {
+        openModal && (
+          <div className="open__modal">
+            <div
+              onClick={() => setOpenModal(!openModal)}
+              className="close__modal">X</div>
+          </div>
+        )
+      }
+    </>
+  )
+}
+
+export default App
